e7aa16d9853cfbff3351ffacd267be16589958f4
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-04-26 03:57:07 -0400

    Add support for custom headers and footers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def add_spaces(value, num):
         value = value + " "
     return value
 
-def no_split(column_lengths, retain_headers, retain_footers, ext_txt):
+def no_split(column_lengths, retain_headers, retain_footers, ext_txt, custom_header, custom_footer):
     for filename in os.listdir(os.getcwd()):
         if filename.endswith(".xlsx"):  
             myfile = xlrd.open_workbook(filename)
@@ -25,8 +25,11 @@ def no_split(column_lengths, retain_headers, retain_footers, ext_txt):
             mysheet = myfile.sheet_by_index(0)
             if (retain_headers == "yes"):
                 header = str(mysheet.cell_value(0, 0))
-            new.write(header)
-            new.write("\n")
+                new.write(header)
+                new.write("\n")
+            if (custom_header != "no"):
+                new.write(custom_header)
+                new.write("\n")
             for rownum in range(mysheet.nrows):
                 for columnnum in range(len(column_lengths)):
                     value = str(mysheet.cell_value(rownum, columnnum))
@@ -38,8 +41,12 @@ def no_split(column_lengths, retain_headers, retain_footers, ext_txt):
                 for key in batches:
                     curr = batches.get(key)
                     curr.write(footer)
+            if (custom_footer != "no"):
+                for key in batches:
+                    curr = batches.get(key)
+                    curr.write(custom_footer)
 
-def split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_txt):
+def split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_txt, custom_header, custom_footer):
     batches = {}
     for filename in os.listdir(os.getcwd()):
         if filename.endswith(".xlsx"):  
@@ -69,8 +76,12 @@ def split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_t
                         new_file = "output/" + filename[:-5] + "-BATCH " + str(batch_num) + ".ext"
                     batches[batch_num] = open(new_file,"w")
                     new = batches.get(batch_num)
-                    new.write(header)
-                    new.write("\n")
+                    if (retain_headers == "yes"):
+                        new.write(header)
+                        new.write("\n")
+                    if (custom_header != "no"):
+                        new.write(custom_header)
+                        new.write("\n")
                     for columnnum in range(len(column_lengths)):
                         value = str(mysheet.cell_value(rownum, columnnum))
                         value = add_spaces(value, column_lengths[columnnum])
@@ -81,6 +92,10 @@ def split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_t
                 for key in batches:
                     curr = batches.get(key)
                     curr.write(footer)
+            if (custom_footer != "no"):
+                for key in batches:
+                    curr = batches.get(key)
+                    curr.write(custom_footer)
 
 path = os.getcwd() + "/output"
 try:
@@ -88,38 +103,24 @@ try:
 except OSError:
     shutil.rmtree(path)
     os.mkdir(path)
-config = input("Config mode or input mode? Enter c for config or i for input? ")
-if config == "i":
-    num_col = int(input("How many columns are there?: "))
-    column_lengths = [None] * num_col
-    for i in range(num_col):
-        column_lengths[i] = input("What is the length of column " + str(i) + "?: ")
-
-    num_split = int(input("Does this need to be split into batches? If so, please enter the column number on which to split (Enter -1 for n/a): "))
-    retain_headers = input("Does this need to retain  headers and footers? (yes/no)")
-    retain_footers = input("Does this need to retain footers? (yes/no)")
-    ext_txt = input("Should the output file be an ext or txt? (ext/txt")
-    if (num_split == -1):
-        no_split(column_lengths, retain_headers, retain_footers, ext_txt)
-    else:
-        split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_txt)
+config = configparser.ConfigParser()
+config.read("config.ini")
+column_lengths = config['Main']['ColumnLengths'].split()
+retain_headers = config['Main']['RetainHeader']
+retain_footers = config['Main']['RetainFooter']
+ext_txt = config['Main']['ExtOrTxt']
+custom_header = config['Main']['CustomHeader']
+custom_footer = config['Main']['CustomFooter']
+#config_file = open("config.txt", "r")
+#config_lines = config_file.read().splitlines()
+#column_lengths = config_lines[0].split()
+#retain_headers = config_lines[2]
+#retain_footers = config_lines[3]
+#ext_txt = config_lines[4]
+for i in range(len(column_lengths)):
+    column_lengths[i] = int(column_lengths[i])
+num_split = int(config['Main']['SplitColumn'])
+if (num_split == -1):
+    no_split(column_lengths, retain_headers, retain_footers, ext_txt, custom_header, custom_footer)
 else:
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    column_lengths = config['Main']['ColumnLengths'].split()
-    retain_headers = config['Main']['RetainHeader']
-    retain_footers = config['Main']['RetainFooter']
-    ext_txt = config['Main']['ExtOrTxt']
-    #config_file = open("config.txt", "r")
-    #config_lines = config_file.read().splitlines()
-    #column_lengths = config_lines[0].split()
-    #retain_headers = config_lines[2]
-    #retain_footers = config_lines[3]
-    #ext_txt = config_lines[4]
-    for i in range(len(column_lengths)):
-        column_lengths[i] = int(column_lengths[i])
-    num_split = int(config['Main']['SplitColumn'])
-    if (num_split == -1):
-        no_split(column_lengths, retain_headers, retain_footers, ext_txt)
-    else:
-        split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_txt)
+    split_batch(column_lengths, num_split, retain_headers, retain_footers, ext_txt, custom_header, custom_footer)
